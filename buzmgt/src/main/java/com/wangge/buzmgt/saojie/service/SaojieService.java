d0b483f7955bd966909e848902b7e0d57eac4ecd
yinxunguo <yinxunguo@yinxunguo>
2016-01-15 18:49:04 +0800

    11

--- buzmgt/src/main/java/com/wangge/buzmgt/saojie/service/SaojieService.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/saojie/service/SaojieService.java
@@ -1,10 +1,12 @@
 package com.wangge.buzmgt.saojie.service;
 
+import org.springframework.data.domain.Page;
+
 import com.wangge.buzmgt.saojie.entity.Saojie;
 
 public interface SaojieService {
 
   void saveSaojie(Saojie saojie);
 
-  
+  Page<Saojie> getSaojieList(Saojie saojie,int pageNum);
 }
--- buzmgt/src/main/java/com/wangge/buzmgt/saojie/service/SaojieServiceImpl.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/saojie/service/SaojieServiceImpl.java
@@ -1,19 +1,77 @@
 package com.wangge.buzmgt.saojie.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import com.wangge.buzmgt.region.entity.Region;
+import com.wangge.buzmgt.salesman.entity.salesMan;
 import com.wangge.buzmgt.saojie.entity.Saojie;
+import com.wangge.buzmgt.saojie.entity.Saojie.SaojieStatus;
 import com.wangge.buzmgt.saojie.repository.SaojieRepository;
 
 @Service
 public class SaojieServiceImpl implements SaojieService {
 
-	@Resource
-	private SaojieRepository saojieRepository;
+  @Resource
+  private SaojieRepository saojieRepository;
 
   @Override
   public void saveSaojie(Saojie saojie) {
     saojieRepository.save(saojie);
   }
-	
+
+  @Override
+  @Transactional
+  public Page<Saojie> getSaojieList(Saojie saojie, int pageNum) {
+    return saojieRepository.findAll(new Specification<Saojie>() {
+
+      public Predicate toPredicate(Root<Saojie> root, CriteriaQuery<?> query,
+          CriteriaBuilder cb) {
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        if (saojie.getSalesman() != null) {
+          Join<Saojie, salesMan> salesmanJoin = root.join(root.getModel()
+              .getSingularAttribute("salesman", salesMan.class), JoinType.LEFT);
+          Predicate p1 = cb.like(salesmanJoin.get("truename").as(String.class),
+              "%" + saojie.getSalesman().getTruename() + "%");
+          Predicate p2 = cb.equal(salesmanJoin.get("jobNum").as(String.class),
+              saojie.getSalesman().getJobNum());
+          predicates.add(cb.or(p1, p2));
+        }
+        if (saojie.getStatus() != null) {
+          predicates.add(cb.equal(root.get("status").as(SaojieStatus.class),
+              saojie.getStatus()));
+        }
+        /*
+         * else{
+         * predicates.add(cb.equal(root.get("status").as(SaojieStatus.class),
+         * SaojieStatus)); }
+         */
+
+        if (saojie.getRegion() != null) {
+          Join<Saojie, Region> regionJoin = root.join(root.getModel()
+              .getSingularAttribute("region", Region.class), JoinType.LEFT);
+          predicates.add(cb.equal(regionJoin.get("id").as(String.class), saojie
+              .getRegion().getId()));
+        }
+        return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+      }
+
+    }, new PageRequest(pageNum, 2));
+
+  }
 }
