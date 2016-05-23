8d6a02d722f28e508b2396366ff646d53e602735
jiaoyue <jiaoyue_jn@163.com>
2016-05-23 21:44:17 +0800

    加了注释

--- buzmgt/src/main/java/com/wangge/buzmgt/oil/entity/OilParameters.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/oil/entity/OilParameters.java
@@ -1,22 +1,11 @@
 
 package com.wangge.buzmgt.oil.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
+import com.wangge.buzmgt.region.entity.Region;
 import org.hibernate.annotations.GenericGenerator;
 
-import com.wangge.buzmgt.region.entity.Region;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * 油补系数
@@ -30,7 +19,7 @@ public class OilParameters implements Serializable{
 	
 	@Id 
 	@GenericGenerator(name="idgen",strategy="increment")
-	@GeneratedValue(generator="idgen")//自定义生成主键策略
+	@GeneratedValue(generator="idgen")   //自定义生成主键策略
 	@Column(name="ID")
 	private Long id;//主键
 	
