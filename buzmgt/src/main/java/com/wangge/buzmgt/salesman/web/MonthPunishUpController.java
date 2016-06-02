6d927662b0cf171235477961ee79ee8ef9586088
leedsheep <jiaoyue_jn@163.com>
2016-06-02 17:45:07 +0800

    jsonformat过滤无用的字段

--- buzmgt/src/main/java/com/wangge/buzmgt/salesman/web/MonthPunishUpController.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/salesman/web/MonthPunishUpController.java
@@ -1,11 +1,10 @@
 package com.wangge.buzmgt.salesman.web;
 
-import com.wangge.buzmgt.jsonformat.JSONFormat;
+import com.alibaba.fastjson.JSONObject;
 import com.wangge.buzmgt.salesman.entity.MonthPunishUp;
 import com.wangge.buzmgt.salesman.repository.MothPunishUpRepository;
 import com.wangge.buzmgt.salesman.service.MonthPunishUpService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,12 +13,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 /**
  * Created by 神盾局 on 2016/5/21.
- */@Controller
+ */
+@Controller
 @RequestMapping("/MonthPunishUp")
 public class MonthPunishUpController {
 
@@ -28,80 +29,112 @@ public class MonthPunishUpController {
     @Autowired
     private MothPunishUpRepository mothPunishUpRepository;
 
-    @RequestMapping(value="/list",method = RequestMethod.GET)
-    public String toList(){
-        return "punishset/month_punish_record";
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String toList() {
+        return "punishset/month_punish_record2";
     }
 
 
-
-    @RequestMapping(value="/MonthPunishUps",method= RequestMethod.GET)
+//    @RequestMapping(value="/MonthPunishUps",method= RequestMethod.GET)
 //    @JSONFormat(filterField = {"SalesMan.user","region.parent","region.children"})
-    @JSONFormat(filterField = {"SalesMan.user","region.children"})
-    public
-    Page<MonthPunishUp> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                @RequestParam(value = "size", defaultValue = "3") Integer size){
-        System.out.println("*************");
+////    @JSONFormat(filterField = {"SalesMan.user","region.children"})
+////    @ResponseBody
+//    public  List findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+//                                       @RequestParam(value = "size", defaultValue = "3") Integer size){
+//        System.out.println("*************");
+//        Sort sort = new Sort(Sort.Direction.DESC, "id");
+//        Pageable pageable = new PageRequest(page, size, sort);
+//        List list = new ArrayList<>();
+//        list.add(service.findByPage(pageable));
+//        list.add("测试1");
+//        list.add("测试2");
+//        Page p = service.findByPage(pageable);
+//        return list;
+//    }
+
+
+//    @RequestMapping(value="/MonthPunishUps",method= RequestMethod.GET)
+////    @JSONFormat(filterField = {"SalesMan.user","region.parent","region.children"})
+//    @JSONFormat(filterField = {"SalesMan.user","region.children"})
+//    public ResponseEntity<JsonResponse> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+//                                                @RequestParam(value = "size", defaultValue = "3") Integer size){
+//        System.out.println("*************");
+//        Sort sort = new Sort(Sort.Direction.DESC, "id");
+//        Pageable pageable = new PageRequest(page, size, sort);
+//        return   new ResponseEntity<JsonResponse>(new JsonResponse(service.findByPage(pageable)), HttpStatus.OK) ;
+//    }
+
+
+    @RequestMapping(value = "/MonthPunishUps", method = RequestMethod.GET)
+//    @JSONFormat(filterField = {"SalesMan.user", "region.children"})
+    @ResponseBody
+    public JSONObject findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                       @RequestParam(value = "size", defaultValue = "3") Integer size,
+                                       @RequestParam(value = "startTime", defaultValue = "2016-01-26") String startTime,
+                                       @RequestParam(value = "endTime", defaultValue = "2016-07-26") String endTime) {
+        System.out.println("page: " + page + "   " + "size:" + size);
+        System.out.println("startTime: " + startTime + "   " + "endTime:" + endTime);
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         Pageable pageable = new PageRequest(page, size, sort);
-
-        return  service.findByPage(pageable);
+        Float sum = mothPunishUpRepository.amerceSum();
+        JSONObject result = new JSONObject();
+        result.put("sum",sum);
+        result.put("data",service.findByPage(startTime, endTime, pageable));
+        return result;
     }
 
 
-
-
-    @RequestMapping(value="/list1",method = RequestMethod.GET)
-    public String ceshi(Model model){
+    @RequestMapping(value = "/list1", method = RequestMethod.GET)
+    public String ceshi(Model model) {
         List<MonthPunishUp> list = mothPunishUpRepository.findAll();
-        model.addAttribute("list",list);
+        model.addAttribute("list", list);
         return "punishset/month_punish_record";
     }
 
     /**
      * 暂时用这个
+     *
      * @param page
      * @param size
      * @param model
      * @return
      */
-    @RequestMapping(value="/list2",method = RequestMethod.GET)
+    @RequestMapping(value = "/list2", method = RequestMethod.GET)
     public String findAll1(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                @RequestParam(value = "size", defaultValue = "3") Integer size,Model model){
+                           @RequestParam(value = "size", defaultValue = "3") Integer size, Model model) {
         System.out.println("*************");
         Float sum = mothPunishUpRepository.amerceSum();
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         Pageable pageable = new PageRequest(page, size, sort);
         model.addAttribute("page", service.findByPage(pageable));
-        model.addAttribute("sum",sum);
-        return  "punishset/month_punish_record1";
+        model.addAttribute("sum", sum);
+        return "punishset/month_punish_record1";
     }
-
-
 
     /**
      * 模糊查询
+     *
      * @param page
      * @param size
      * @param model
      * @return
      */
-    @RequestMapping(value="/list3",method = RequestMethod.GET)
+    @RequestMapping(value = "/list3", method = RequestMethod.GET)
     public String findAll2(@RequestParam(value = "page", defaultValue = "0") Integer page,
                            @RequestParam(value = "size", defaultValue = "3") Integer size,
-                           @RequestParam(value = "startTime", defaultValue = "2016-01-26")String startTime,
+                           @RequestParam(value = "startTime", defaultValue = "2016-01-26") String startTime,
                            @RequestParam(value = "endTime", defaultValue = "2016-07-26") String endTime,
-                           Model model){
+                           Model model) {
 
-        System.out.println(startTime+"**********"+endTime);
-        model.addAttribute("startTimeFen",startTime);
-        model.addAttribute("endTimeFen",endTime);
+        System.out.println(startTime + "**********" + endTime);
+        model.addAttribute("startTimeFen", startTime);
+        model.addAttribute("endTimeFen", endTime);
         Float sum = mothPunishUpRepository.amerceSum();
         Sort sort = new Sort(Sort.Direction.DESC, "createDate");
         Pageable pageable = new PageRequest(page, size, sort);
-        model.addAttribute("page", service.findByPage(startTime,endTime,pageable));
-        model.addAttribute("sum",sum);
-        return  "punishset/month_punish_record1";
+        model.addAttribute("page", service.findByPage(startTime, endTime, pageable));
+        model.addAttribute("sum", sum);
+        return "punishset/month_punish_record1";
 
     }
 }
