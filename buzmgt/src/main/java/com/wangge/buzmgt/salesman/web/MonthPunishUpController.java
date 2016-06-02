2eebc631785cb64b9705249a429867fa0d9778b6
leedsheep <jiaoyue_jn@163.com>
2016-06-02 18:23:36 +0800

    修改controller

--- buzmgt/src/main/java/com/wangge/buzmgt/salesman/web/MonthPunishUpController.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/salesman/web/MonthPunishUpController.java
@@ -1,12 +1,10 @@
 package com.wangge.buzmgt.salesman.web;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wangge.buzmgt.salesman.entity.MonthPunishUp;
 import com.wangge.buzmgt.salesman.repository.MothPunishUpRepository;
 import com.wangge.buzmgt.salesman.service.MonthPunishUpService;
-import com.wangge.json.JSONFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -31,22 +30,42 @@ public class MonthPunishUpController {
 
     @RequestMapping(value="/list",method = RequestMethod.GET)
     public String toList(){
-        return "punishset/month_punish_record";
+        return "punishset/month_punish_record2";
     }
 
 
 
+//    @RequestMapping(value="/MonthPunishUps",method= RequestMethod.GET)
+////    @JSONFormat(filterField = {"SalesMan.user","region.parent","region.children"})
+//    @JSONFormat(filterField = {"SalesMan.user","region.children"})
+//    public
+//    Page<MonthPunishUp> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+//                                @RequestParam(value = "size", defaultValue = "3") Integer size){
+//        System.out.println("*************");
+//        Sort sort = new Sort(Sort.Direction.DESC, "id");
+//        Pageable pageable = new PageRequest(page, size, sort);
+//
+//        return  service.findByPage(pageable);
+//    }
+
+
     @RequestMapping(value="/MonthPunishUps",method= RequestMethod.GET)
 //    @JSONFormat(filterField = {"SalesMan.user","region.parent","region.children"})
-    @JSONFormat(filterField = {"SalesMan.user","region.children"})
-    public
-    Page<MonthPunishUp> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                @RequestParam(value = "size", defaultValue = "3") Integer size){
+//    @JSONFormat(filterField = {"SalesMan.user","region.children"})
+    public @ResponseBody JSONObject findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                       @RequestParam(value = "size", defaultValue = "3") Integer size,
+                       @RequestParam(value = "startTime", defaultValue = "2016-01-26")String startTime,
+                       @RequestParam(value = "endTime", defaultValue = "2016-07-26") String endTime
+                                ){
         System.out.println("*************");
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         Pageable pageable = new PageRequest(page, size, sort);
+        Float sum = mothPunishUpRepository.amerceSum();
+        JSONObject result = new JSONObject();
+        result.put("sum",sum);
+        result.put("data",service.findByPage(startTime,endTime,pageable));
 
-        return  service.findByPage(pageable);
+        return result;
     }
 
 
