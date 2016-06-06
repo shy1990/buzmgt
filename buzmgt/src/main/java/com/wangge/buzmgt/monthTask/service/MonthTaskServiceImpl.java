1f387085806c615e980415575b3bb3901038b767
yangqc <aqicaij@qq.com>
2016-06-06 15:01:36 +0800

    更改定时任务执行时间

--- buzmgt/src/main/java/com/wangge/buzmgt/monthTask/service/MonthTaskServiceImpl.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/monthTask/service/MonthTaskServiceImpl.java
@@ -85,18 +85,25 @@ public class MonthTaskServiceImpl implements MonthTaskService {
 	private static Integer[] levels = new Integer[] { 20, 15, 10, 7, 4 };
 
 	// 订单支付状态为1,计算上个月的订单情况
-	public static final String lsdatasql = "select  r.registdata_id, r.region_id , o3.days, r.shop_name, '1' month \n"
-			+ "  from (select count(1) days, o2.member_id\n" + "          from (select o1.member_id, o1.day\n"
-			+ "                  from (select t.member_id,\n" + "                               t.CREATETIME,\n"
-			+ "                               to_char(t.CREATETIME, 'yyyy-mm-dd') day,\n"
-			+ "                               t.ship_Name\n"
-			+ "                          from SJZAIXIAN.SJ_TB_ORDER t\n"
-			+ "                         where to_char(t.createTime, 'yyyy-mm') =\n"
-			+ "                               to_char(sysdate - interval '1' month, 'yyyy-mm')\n"
-			+ "                           and t.pay_status = '1') o1\n"
-			+ "                 group by o1.member_id, o1.day) o2\n" + "         group by o2.member_id\n"
-			+ "         order by days desc) o3\n" + "  left join SJZAIXIAN.SJ_TB_members m on o3.member_id = m.id \n"
-			+ "   inner join  sys_registdata r on r.member_id=m.id \n" + " where  r.user_id like '$town%' ";
+	public static final String lsdatasql =
+
+			"select r.registdata_id, r.region_id, o3.days, r.shop_name, '1' month\n" + "  from sys_registdata r\n"
+					+ "  left join SJZAIXIAN.SJ_TB_members m on r.member_id = m.id\n"
+					+ "  left join (select count(1) days, o2.member_id\n"
+					+ "               from (select o1.member_id, o1.day\n"
+					+ "                       from (select t.member_id,\n"
+					+ "                                    t.CREATETIME,\n"
+					+ "                                    to_char(t.CREATETIME, 'yyyy-mm-dd') day,\n"
+					+ "                                    t.ship_Name\n"
+					+ "                               from SJZAIXIAN.SJ_TB_ORDER t\n"
+					+ "                              where to_char(t.createTime, 'yyyy-mm') =\n"
+					+ "                                    to_char(sysdate - interval '1' month,\n"
+					+ "                                            'yyyy-mm')\n"
+					+ "                                and t.pay_status = '1') o1\n"
+					+ "                      group by o1.member_id, o1.day) o2\n"
+					+ "              group by o2.member_id\n"
+					+ "              order by days desc) o3 on o3.member_id = m.id\n"
+					+ " where r.user_id like '$town%'";
 	// 上月拜访访情况
 	public static final String lsVisitSql = "select count(1), tmp.month, tmp.registdata_id, tmp.region_id \n"
 			+ "  from (select substr(g.finish_time, 0, 7) month,\n" + "               g.registdata_id,\n"
@@ -106,8 +113,7 @@ public class MonthTaskServiceImpl implements MonthTaskService {
 			+ "                  from sys_visit v\n"
 			+ "                  left join sys_registdata r on v.registdata_id =\n"
 			+ "                                                r.registdata_id\n"
-			+ "                 where v.finish_time is not null\n"
-			+ "                   and r.user_id  like '$town%'\n"
+			+ "                 where v.finish_time is not null\n" + "                   and r.user_id  like '$town%'\n"
 			+ "                   and (to_char(v.finish_time, 'yyyy-mm') =\n"
 			+ "                       to_char(sysdate - interval '1' month, 'yyyy-mm'))) g\n"
 			+ "         group by g.finish_time, g.registdata_id, g.region_id) tmp\n"
