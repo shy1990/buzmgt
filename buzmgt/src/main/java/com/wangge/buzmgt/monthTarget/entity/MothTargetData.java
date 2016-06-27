ae7cb7689716ec5bbbf4e5c2f20f4dc739298356
leadsheepjoe <jiaoyue_jn@163.com>
2016-06-27 19:59:14 +0800

    月任务查询

---
+++ buzmgt/src/main/java/com/wangge/buzmgt/monthTarget/entity/MothTargetData.java
@@ -0,0 +1,140 @@
+package com.wangge.buzmgt.monthTarget.entity;
+
+import com.wangge.buzmgt.region.entity.Region;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by joe on 16-6-27.
+ *
+ * 视图实体类（MothTargetData）
+ */
+//@Entity
+//@Immutable
+//@Subselect("select * from mothtargetdata")
+public class MothTargetData implements Serializable {
+
+    private String orderId;//订单id
+
+    private String memberId;//订单项id
+
+    private Integer nums;//商品数量
+
+    private Date createTime;//订单时间
+
+    private String regionId;//所属区域
+
+    private String phoneNmu;//商家电话
+
+    private String shopName;//商家名称
+
+    private BigDecimal count;//提货次数
+
+    private BigDecimal numsOne;//单品数量
+
+    public BigDecimal getCount() {
+        return count;
+    }
+
+    public void setCount(BigDecimal count) {
+        this.count = count;
+    }
+
+    public BigDecimal getNumsOne() {
+        return numsOne;
+    }
+
+    public void setNumsOne(BigDecimal numsOne) {
+        this.numsOne = numsOne;
+    }
+
+    private Region region;//区域
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    public MothTargetData(){};
+
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public Integer getNums() {
+        return nums;
+    }
+
+    public void setNums(Integer nums) {
+        this.nums = nums;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(String regionId) {
+        this.regionId = regionId;
+    }
+
+    public String getPhoneNmu() {
+        return phoneNmu;
+    }
+
+    public void setPhoneNmu(String phoneNmu) {
+        this.phoneNmu = phoneNmu;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    @Override
+    public String toString() {
+        return "MothTargetData{" +
+                "orderId='" + orderId + '\'' +
+                ", memberId='" + memberId + '\'' +
+                ", nums=" + nums +
+                ", createTime=" + createTime +
+                ", regionId='" + regionId + '\'' +
+                ", phoneNmu='" + phoneNmu + '\'' +
+                ", shopName='" + shopName + '\'' +
+                ", count=" + count +
+                ", numsOne=" + numsOne +
+                ", region=" + region +
+                '}';
+    }
+}
---
+++ buzmgt/src/test/java/com/wangge/buzmgt/monthTarget/service/MothTargetDataServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.wangge.buzmgt.monthTarget.service;
+
+import com.wangge.buzmgt.BuzmgtApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by joe on 16-6-27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BuzmgtApplication.class)
+@WebAppConfiguration
+public class MothTargetDataServiceImplTest {
+    @Autowired
+    private MothTargetDataService mothTargetDataService;
+    @Test
+    public void getMothTargetDatas() throws Exception {
+        mothTargetDataService.getMothTargetDatas("2015-02-15",0,9);
+    }
+
+}
