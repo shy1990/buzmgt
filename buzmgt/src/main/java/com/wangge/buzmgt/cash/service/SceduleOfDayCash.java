e7bfd802dbb307f194ce68efe54cf4b2285b3bfa
ChenGuop <15665788175@163.com>
2016-07-20 11:56:56 +0800

    定时结算

---
+++ buzmgt/src/main/java/com/wangge/buzmgt/cash/service/SceduleOfDayCash.java
@@ -0,0 +1,37 @@
+package com.wangge.buzmgt.cash.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class SceduleOfDayCash {
+	@PersistenceContext
+	private EntityManager em;
+	@Autowired
+	private CashService cashService;
+
+	private Logger logger=Logger.getLogger(SceduleOfDayCash.class);
+	// 每日23时30分执行 0 30 23 * * ? 
+	@Scheduled(cron = " 0 30 23 * * ? ")
+	public void handleCashData() {
+	  logger.info("-----------------开启定时结算");
+	  List<String> userIds=cashService.findByStatusGroupByUserId();
+	  userIds.forEach(userId->{
+	    logger.info("userId:"+userId);
+	    boolean msg=cashService.createWaterOrderByCash(userId);
+	    logger.info("返回结果:"+msg);
+	  });
+	  logger.info("-----------------结束定时结算");
+	  
+	}
+
+}
---
+++ buzmgt/src/main/java/com/wangge/buzmgt/cash/service/WaterOrderDetialService.java
@@ -0,0 +1,25 @@
+package com.wangge.buzmgt.cash.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.wangge.buzmgt.cash.entity.WaterOrderCash;
+import com.wangge.buzmgt.cash.entity.WaterOrderDetail;
+
+public interface WaterOrderDetialService {
+  
+  public WaterOrderDetail findByOrderNo(String orderNo);
+  
+  public void save(WaterOrderDetail waterOrderDetail);
+  
+  public void save(List<WaterOrderDetail> waterOrderDetails);
+  
+}
+
+  
