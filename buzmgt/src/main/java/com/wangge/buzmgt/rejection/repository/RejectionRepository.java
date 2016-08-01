a7c111c6cc2742ac39fd4538f0663de33924fce5
yinxunguo <yinxunguo@yinxunguo>
2016-08-01 17:43:19 +0800

    拒收落下的

---
+++ buzmgt/src/main/java/com/wangge/buzmgt/rejection/repository/RejectionRepository.java
@@ -0,0 +1,10 @@
+package com.wangge.buzmgt.rejection.repository;
+
+import com.wangge.buzmgt.rejection.entity.Rejection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface RejectionRepository extends JpaRepository<Rejection, Long>,JpaSpecificationExecutor<Rejection> {
+
+
+}
