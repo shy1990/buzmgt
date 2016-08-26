37f6afd215e51d161f74878d132fb89065f79944
yangqc <aqicaij@qq.com>
2016-08-26 16:33:22 +0800

    重新提交该文件

--- buzmgt/src/main/java/com/wangge/buzmgt/income/ywsalary/repository/BaseSalaryRepository.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/income/ywsalary/repository/BaseSalaryRepository.java
@@ -19,9 +19,6 @@ public interface BaseSalaryRepository extends JpaRepository<BaseSalary, Long>, J
   @EntityGraph("user")
   Page<BaseSalary> findAll(Specification<BaseSalary> spec, Pageable page);
   
-  @Override
-  @EntityGraph("user")
-  List<BaseSalary> findAll(Specification<BaseSalary> spec);
   /**
    * findByFlagAndUser_Id:查找历史记录. <br/>
    * 
@@ -34,40 +31,5 @@ public interface BaseSalaryRepository extends JpaRepository<BaseSalary, Long>, J
   
   @Query("select max(s.times) from BaseSalary s where s.userId=?1")
   Optional<Integer> findMaxTimesByUserId(String userId);
-  
-}
-=======
-package com.wangge.buzmgt.income.ywsalary.repository;
-
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-
-import com.wangge.buzmgt.common.FlagEnum;
-import com.wangge.buzmgt.income.ywsalary.entity.BaseSalary;
-
-public interface BaseSalaryRepository extends JpaRepository<BaseSalary, Long>, JpaSpecificationExecutor<BaseSalary> {
-  @Override
-  @EntityGraph("user")
-  Page<BaseSalary> findAll(Specification<BaseSalary> spec, Pageable page);
-  
-  /**
-   * findByFlagAndUser_Id:查找历史记录. <br/>
-   * 
-   * @author yangqc
-   * @param id
-   * @return
-   * @since JDK 1.8
-   */
-  List<BaseSalary> findByFlagAndUser_Id(FlagEnum flag, String id);
-  
-  @Query("select max(s.times) from BaseSalary s where s.userId=?1")
-  Integer findMaxTimesByUserId(String userId);
   
 }
