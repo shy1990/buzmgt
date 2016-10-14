8dc08f29cb961a203f26266358e068f8653f7efc
yangqc <aqicaij@qq.com>
2016-10-14 14:59:16 +0800

    修改hedgevo的实体

--- buzmgt/src/main/java/com/wangge/buzmgt/income/main/entity/IncomeSubError.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/income/main/entity/IncomeSubError.java
@@ -29,9 +29,9 @@ public class IncomeSubError {
    * 错误类型:0 达量设置 1 品牌型号 2 价格区间3叠加奖励 4.达量奖励 ,<br/>
    * 5:50计算总工资,51计算油补;<br/>
    * 7:71定时任务执行出错;<br/>
-   * 
+   * 计算类型:0:收益计算;1.售后冲减,2.其他情况
    */
-  private Integer type;
+  private Integer type,calcuType=2;
   // 关联主键,规则id,或定时任务id
   private Long keyId;
   
@@ -101,8 +101,27 @@ public class IncomeSubError {
     this.keyId = keyId;
   }
   
+
+  public Integer getCalcuType() {
+    return calcuType;
+  }
+
+  public void setCalcuType(Integer calcuType) {
+    this.calcuType = calcuType;
+  }
+
   public IncomeSubError(String orderno, String userId, String errorInfo, String goodId, Integer type, Long keyId) {
     super();
+    this.orderno = orderno;
+    this.userId = userId;
+    this.errorInfo = errorInfo;
+    this.goodId = goodId;
+    this.type = type;
+    this.keyId = keyId;
+  }
+  public IncomeSubError(Integer calCutype,String orderno, String userId, String errorInfo, String goodId, Integer type, Long keyId) {
+    super();
+    this.calcuType=calCutype;
     this.orderno = orderno;
     this.userId = userId;
     this.errorInfo = errorInfo;
