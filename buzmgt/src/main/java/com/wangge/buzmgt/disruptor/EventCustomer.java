fea1296bc83419666b9bcaff7da923d3bb268990
954691592@qq.com <954691592@qq.com>
2016-10-26 16:47:46 +0800

    出库消费者

--- buzmgt/src/main/java/com/wangge/buzmgt/disruptor/EventCustomer.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/disruptor/EventCustomer.java
@@ -1,16 +1,21 @@
 package com.wangge.buzmgt.disruptor;
 
 import com.lmax.disruptor.EventHandler;
+import com.wangge.buzmgt.income.main.service.MainIncomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**消费者
  * Created by jiabin on 16-9-7.
  */
   public class EventCustomer implements EventHandler<MallOrder>{
-
+  @Autowired
+  private MainIncomeService mainIncomeService;
 
   @Override
   public void onEvent(MallOrder mallOrder, long l, boolean b) throws Exception {
 
     System.out.println("ordernum:"+mallOrder.getOrderNum()+"*****memberid"+mallOrder.getMember_id());
+
+    mainIncomeService.caculateOutedOrder(mallOrder.getOrderNum());
   }
 }
