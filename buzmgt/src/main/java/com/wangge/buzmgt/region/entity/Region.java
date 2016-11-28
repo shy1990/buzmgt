14cbf0fdc804a1bf472be7a06054196f40e7bd20
peter <1432193885@qq.com>
2016-11-28 10:41:06 +0800

    解决区域

--- buzmgt/src/main/java/com/wangge/buzmgt/region/entity/Region.java
+++ buzmgt/src/main/java/com/wangge/buzmgt/region/entity/Region.java
@@ -1,34 +1,13 @@
 package com.wangge.buzmgt.region.entity;
 
 
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Collections;
-
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.SecondaryTable;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 @JsonInclude(Include.NON_EMPTY)
 @Entity
 @Table(name = "SYS_REGION")
@@ -36,114 +15,128 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonIgnoreProperties(value = { "hibernateLazyInitializer" ,"handler"})
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Region implements Serializable {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	public static enum RegionType {
-		COUNTRY("国"), PARGANA("大区"), PROVINCE("省"), AREA("区"), CITY("市"), COUNTY("县"), TOWN("镇"), OTHER("其他");
-		private String name;
+  @Id
+  @Column(name = "REGION_ID")
+  private String id;
+  private String name;
+  
+  @Lob
+  @Basic(fetch = FetchType.LAZY)
+  @Column(table = "SYS_COORDINATES", columnDefinition = "CLOB", name = "content",updatable=true)
+  private String coordinates;
 
-		RegionType(String name) {
-			this.name = name;
-		}
 
-		public String getName() {
-			return name;
-		}
-	}
 
-	@Id
-	@Column(name = "REGION_ID")
-	private String id;
-	private String name;
-	
-	@Lob
-	@Basic(fetch = FetchType.LAZY)
-	@Column(table = "SYS_COORDINATES", columnDefinition = "CLOB", name = "content",updatable=true)
-	private String coordinates;
+  @JsonIgnore
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "PARENT_ID")
+  private Region parent;
 
-	@Enumerated(EnumType.ORDINAL)
-	private RegionType type;
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
+  private Collection<Region> children;
 
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "PARENT_ID")
-	private Region parent;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "REGION_TYPE_ID")
+  private RegionType type;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
-	private Collection<Region> children;
-
-	@JoinColumn(name = "CENTER_POINT")
+ private String namepath;
+  
   private String centerPoint;
-	
-	public String getId() {
-		return id;
-	}
 
-	public Region() {
-		super();
-	}
+  private Integer starsLevel;
 
-	/**
-	 * 区域编号
-	 * 
-	 * @param id
-	 * @param name
-	 */
-	public Region(String id, String name, RegionType type) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.type = type;
-	}
 
-	public RegionType getType() {
-		return type;
-	}
+  public String getId() {
+    return id;
+  }
 
-	public void setType(RegionType type) {
-		this.type = type;
-	}
+  public Region() {
+    super();
+  }
 
-	/**
-	 * 区域编号
-	 * 
-	 * @param id
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
+  /**
+   * 区域编号
+   * 
+   * @param id
+   * @param name
+   */
+  public Region(String id, String name, RegionType type) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.type = type;
+  }
 
-	public String getName() {
-		return name;
-	}
+  
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public String getNamepath() {
+    return namepath;
+  }
 
-	public Region getParent() {
-		return parent;
-	}
+  public void setNamepath(String namepath) {
+    this.namepath = namepath;
+  }
 
-	public void setParent(Region parent) {
-		this.parent = parent;
-	}
+  public RegionType getType() {
+    return type;
+  }
 
-	public String getCoordinates() {
-		return coordinates;
-	}
+  public void setType(RegionType type) {
+    this.type = type;
+  }
 
-	public void setCoordinates(String coordinates) {
-		this.coordinates = coordinates;
-	}
+  /**
+   * 区域编号
+   * 
+   * @param id
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
 
-	public Collection<Region> getChildren() {
-		return Collections.unmodifiableCollection(children);
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setChildren(Collection<Region> children) {
-		this.children = children;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Region getParent() {
+    return parent;
+  }
+
+  public void setParent(Region parent) {
+    this.parent = parent;
+  }
+
+  public String getCoordinates() {
+    return coordinates;
+  }
+
+  public void setCoordinates(String coordinates) {
+    this.coordinates = coordinates;
+  }
+
+  
+
+  public Integer getStarsLevel() {
+    return starsLevel;
+  }
+
+  public void setStarsLevel(Integer starsLevel) {
+    this.starsLevel = starsLevel;
+  }
+
+  public Collection<Region> getChildren() {
+    return Collections.unmodifiableCollection(children);
+  }
+
+  public void setChildren(Collection<Region> children) {
+    this.children = children;
+  }
 
   public String getCenterPoint() {
     return centerPoint;
@@ -153,8 +146,5 @@ public class Region implements Serializable {
     this.centerPoint = centerPoint;
   }
 
-  
-	
-	
-	
+
 }
